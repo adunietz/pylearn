d1bbb720ad4977e5b4e5c562dac56190b27663e9
adunietz <anna.dunietz@gmail.com>
2018-01-10 14:43:54 +0200

    variable section

---
+++ variablesection.py
@@ -0,0 +1,17 @@
+# Section 3, Lecture 8
+
+# number = 1
+# print(number)
+# print(type(number))
+#
+# number = "hello"
+# print(type(number))
+
+# Section 3, Lecture 9
+first_number = 1 + 1
+second_number = 105+10
+print(first_number)
+print(second_number)
+
+total = first_number + second_number
+print(total)
